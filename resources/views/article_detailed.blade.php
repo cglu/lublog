b5602d69086dc3cf29ac1b066b02ed230b2f16b1
cglu <shouhuni.xue@qq.com>
2015-10-07 20:08:58 +0900

    变更评论插件
    如题

--- resources/views/article_detailed.blade.php
+++ resources/views/article_detailed.blade.php
@@ -12,17 +12,40 @@
 	{!! $article->content !!}
 </div>
 <hr>
-<!--高速版-->
-<div id="SOHUCS" sid="{{$article->id}}"></div>
-<script charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/changyan.js" ></script>
+<div class="ds-share" data-thread-key="" data-title="{{$article->title}}" data-images="" data-content="{{$article->content}}" data-url="{{url('/article/'.$article->id)}}">
+      <div class="ds-share-inline">
+      <ul  class="ds-share-icons-16">
+      	
+      	<li data-toggle="ds-share-icons-more"><a class="ds-more" href="javascript:void(0);">分享到：</a></li>
+        <li><a class="ds-weibo" href="javascript:void(0);" data-service="weibo">微博</a></li>
+        <li><a class="ds-qzone" href="javascript:void(0);" data-service="qzone">QQ空间</a></li>
+        <li><a class="ds-qqt" href="javascript:void(0);" data-service="qqt">腾讯微博</a></li>
+        <li><a class="ds-wechat" href="javascript:void(0);" data-service="wechat">微信</a></li>
+      	
+      </ul>
+      <div class="ds-share-icons-more">
+      </div>
+    </div>
+</div>
+<!-- 多说评论框 start -->
+	<div class="ds-thread" data-thread-key="{{$article->id}}" data-title="{{$article->title}}" data-url="{{url('/article/'.$article->id)}}"></div>
+<!-- 多说评论框 end -->
+<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
 <script type="text/javascript">
-    window.changyan.api.config({
-        appid: 'cys1AAXeS',
-        conf: 'prod_ffb58d659957774812f5ebcc9649a8c8'
-    });
-</script>       
+var duoshuoQuery = {short_name:"lublog"};
+	(function() {
+		var ds = document.createElement('script');
+		ds.type = 'text/javascript';ds.async = true;
+		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+		ds.charset = 'UTF-8';
+		(document.getElementsByTagName('head')[0] 
+		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+	})();
+	</script>
+<!-- 多说公共JS代码 end -->
+   
 <style>
-#SOHUCS #SOHU_MAIN #powerby_sohu, #SOHUCS #SOHU_MAIN #powerby_sohu .section-service-w, #SOHUCS #SOHU_MAIN #powerby_sohu .section-service-w .service-wrap-w, #SOHUCS #SOHU_MAIN #powerby_sohu .section-service-w .service-wrap-w a {
+.ds-powered-by{
 	/* display: block!important; */
 	text-align: right !important;
 	display: none !important;
