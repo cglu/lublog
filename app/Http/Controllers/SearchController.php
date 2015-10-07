b795d9fed3ae7d5cfa9586cf02d2092d9412a44e
cglu <shouhuni.xue@qq.com>
2015-10-07 11:38:58 +0900

    修复分类检索分页bug
    如题

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -12,7 +12,7 @@ class SearchController extends Controller
     public function searchArticleByCategories(Request $request)
     {
         $id = $request->input('id');
-        $articles = Article::where('category_id', '=', $id)->paginate(1);
+        $articles = Article::where('category_id', '=', $id)->paginate();
         return view('welcome')->with([
             'articles' => $articles,
             'search_param'=>'&id='.$id
