14fad6b6c7ba0b5e0876a4b7dcf768033e592fb7
cglu <shouhuni.xue@qq.com>
2015-10-09 14:57:45 +0900

    修正检索显示排序问题
    如题

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -12,7 +12,7 @@ class SearchController extends Controller
     public function searchArticleByCategories(Request $request)
     {
         $id = $request->input('id');
-        $articles = Article::where('category_id', '=', $id)->paginate();
+        $articles = Article::where('category_id', '=', $id)->orderBy('articles.created_at', 'desc')->paginate();
         return view('welcome')->with([
             'articles' => $articles,
             'search_param'=>'&id='.$id
