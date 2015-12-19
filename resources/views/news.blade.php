4180ef07be4a5f72d5be67e5cb7106bb818a9e25
cglu <shouhuni.xue@qq.com>
2015-12-19 17:36:45 +0800

    文件补漏

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,13 +1,26 @@
 <?php
-use Unicodeveloper\LaravelFeeder\Facades\LaravelFeederFacade;
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 ?>
+
 
 <div class="sidebar-module">
 	<h4>最新IT新闻:</h4>
 	<ol class="list-unstyled">
 <?php
 $url = "http://articles.csdn.net/api/rss.php?tid=1093";
-$rss = simplexml_load_string(file_get_contents($url));
+Log::info("读取CSDN最新的IT新闻");
+if (Cache::has('csdn:rss:1093')) {
+   Log::info("缓存中发现数据，开始从缓存中读取数据.");
+    $html = Cache::get('csdn:rss:1093');
+} else {
+    Log::info("缓存中未发现数据，开始发送HTTP请求。向csdn获取数据");
+    $html = file_get_contents($url);
+    $expires_at=Carbon::now()->addDay(1);
+    Cache::put('csdn:rss:1093',$html,$expires_at);
+}
+$rss = simplexml_load_string($html);
 $news = get_object_vars($rss->channel)['item'];
 $keys = array_rand($news, 5);
 foreach ($keys as $key) {
